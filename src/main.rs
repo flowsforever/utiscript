0497e3a1720488e4232dc3e8b6fbafe19d6261c7
Canmi21 <9997200@qq.com>
2025-03-27 21:22:36 +0800

    fix: clay arg

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
+use std::env;
+
 mod commands;
-use commands::{help/*, build, check, flash, run, summary*/};
+use commands::help;
 
 fn main() {
-    let command = "help";
-
-    match command {
-        //"build" | "b" => build::execute(),
-        //"check" | "c" => check::execute(),
-        //"flash" | "f" | "d" => flash::execute(),
-        //"run" | "r" => run::execute(),
-        "help" | "h" => help::execute(),
-        //"summary" | "s" => summary::execute(),
-        _ => println!("Unknown command!"),
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        help::execute();
+    } else {
+        let command = &args[1];
+        match command.as_str() {
+            //"build" | "b" => build::execute(),
+            //"check" | "c" => check::execute(),
+            //"flash" | "f" | "d" => flash::execute(),
+            //"run" | "r" => run::execute(),
+            "help" | "h" => help::execute(),
+            //"summary" | "s" => summary::execute(),
+            _ => {
+                println!("Unknown command: {}, Try clay help", command);
+            }
+        }
     }
 }
